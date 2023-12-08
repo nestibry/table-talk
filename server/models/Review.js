ba77deef1e452807ebe6e0024b88e9a272172d76
unknown <mhalder9555@gmail.com>
2023-12-08 15:14:59 -0600

    Finished Review model.

---
+++ server/models/Review.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+const reviewSchema = new Schema({
+  photo_id: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  restaurant_name: {
+    type: String,
+    required: true,
+  },
+  restaurant_city: {
+    type: String,
+    required: true,
+  },
+  is_recommended: {
+    type: Boolean,
+    required: true,
+  },
+  creator_id: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  liked_users: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  comments: [commentSchema]
+});
+
+const Review = model('Review', reviewSchema);
+module.exports = Review;
