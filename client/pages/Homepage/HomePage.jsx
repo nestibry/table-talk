ce9610dcc23609f086770de67ffb8d8c1a85ee0a
Veronica Perez <veperez@live.com>
2023-12-10 16:33:03 -0600

    added styling to homepage, css styling  occassionally disconnects

--- client/pages/Homepage/HomePage.jsx
+++ client/pages/Homepage/HomePage.jsx
@@ -5,23 +5,34 @@ import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Auth } from '../../components';
+import dinnerdate from '/images/dinnerdate.png';
 import './HomePage.css';
-import dinnerdate from '../../public/images/dinnerdate.png'
 
 export default function HomePage(){
 
   return (
     <>
-      <h1>Welcome to <span style={{color: '#FFA6D7', fontStyle: 'italic' }}>TableTalk</span></h1>
-      <p> -Where your <span style= {{color: '#C24646'}}>LOVE</span>  for great <span style= {{color:'#F5BE27'}}>FOOD</span> meets the joy of new <span style= {{color:'#F57C36'}}>CONNECTIONS</span> </p>
+
+
+   
+      <h1>Welcome to <span style={{color: '#FFA6D7', fontStyle: 'italic' }}>TableTalk</span></h1><br />
+
+      <h2>
+        Where Your <span style={{ color: '#C24646', fontWeight: '900' }}>LOVE</span> <br />
+        For Great <span style={{ color: '#F5BE27', fontWeight: '900' }}>FOOD</span> <br />
+        Meets the Joy of New <span style={{ color: '#F57C36', fontWeight: '900' }}>CONNECTIONS</span>
+      </h2>
+      <br />
       <Card className='homeCard'>
-      <Card.Body>Want to spice up your social life? Our app is designed for food enthusiasts seeking both delicious meals and meaningful connections. Discover must-try restaurants, swipe through profiles of like-minded individuals, and turn every bite into an opportunity for romance or friendship. Say goodbye to dining alone; join TableTalk for a tasty adventure in food and connection!</Card.Body>
-      <Row>
-        <Col xs={6} md={4}>
-          <Image src={dinnerdate} thumbnail />
+      <Card.Body >
+        <Row>
+        <Col xs={12} md={6}>
+          <p>Want to spice up your social life? Our app is designed for food enthusiasts seeking both delicious meals and meaningful connections. Discover must-try restaurants, swipe through profiles of like-minded individuals, and turn every bite into an opportunity for romance or friendship. Say goodbye to dining alone; join TableTalk for a tasty adventure in food and connection!
+            </p>
         </Col>
-      </Row>
-      <div className="mb-2">
+        <Col xs={12} md={6} className='d-flex flex-column align-items-center'>
+          <Image className="dinnerImg" src={dinnerdate} thumbnail />
+          <div className="mb-2 ">
         <Button  className="signUp" variant="primary" size="lg">
           Sign Up
         </Button>{' '}
@@ -29,12 +40,18 @@ export default function HomePage(){
           How it Works
         </Button>
       </div>
-    </Card>
-    
-    <Auth />
-      <Container>
+        </Col>
 
-    </Container>
+        </Row>
+   
+   
+        </Card.Body>
+ 
+    
+
+    </Card>
+    <Container></Container>
+    <Auth />
 
 
     </>
--- client/pages/HowToPage/HowToPage.jsx
+++ client/pages/HowToPage/HowToPage.jsx
@@ -1,21 +1,67 @@
 import Card from 'react-bootstrap/Card';
-
+import './HowToPage.css';
+import { useInView } from 'react-intersection-observer';
+import React from 'react';
 
 export default function HowToPage(){
+  const [refStepOne, inViewStepOne] = useInView({
+    triggerOnce: true,
+  });
 
+  const [refStepTwo, inViewStepTwo] = useInView({
+    triggerOnce: true,
+    delay: 5000, 
+  });
+
+  const [refStepThree, inViewStepThree] = useInView({
+    triggerOnce: true,
+    delay: 10000, 
+  });
     return (
       <>
-        <h1>How to Page</h1>
-        <p>Step 1</p>
-        <Card>
-        <Card.Body>
+        <h1>Get TableTalking Today!</h1>
+        <Card ref={refStepOne} className={`animated-card ${inViewStepOne ? 'in-view' : ''}`}>
+        <Card.Body className='stepOne'>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            <h2>Step 1</h2>
+            Sign up today by completing a quick profile.
           </Card.Text>
         </Card.Body>
-        <Card.Img variant="bottom" src="holder.js/100px180" />
+           {/* <Card.Img variant="bottom" src="holder.js/100px180" /> */}
       </Card>
-      </>
+      <Card ref={refStepTwo} className={`animated-card ${inViewStepTwo ? 'in-view' : ''}`}>
+        <Card.Body className='stepTwo'>
+          <Card.Text>
+            <h2>Step 2</h2>
+            Pick a status- Are you seeking new friends, a love connection, or just here to find true food love!
+          </Card.Text>
+        </Card.Body>
+           {/* <Card.Img variant="bottom" src="holder.js/100px180" /> */}
+      </Card>
+      <Card ref={refStepThree} className={`animated-card ${inViewStepThree ? 'in-view' : ''}`}>
+        <Card.Body className='stepThree'>
+          <Card.Text>
+            <h2>Step 3</h2>
+            Once your profile is complete, start browsing the news feed for new connections to people or food!
+          </Card.Text>
+        </Card.Body>
+           {/* <Card.Img variant="bottom" src="holder.js/100px180" /> */}
+      </Card>
+      <Card ref={refStepFour} className={`animated-card ${inViewStepFour ? 'in-view' : ''}`}>
+        <Card.Body className='stepFour'>
+          <Card.Text>
+            <h2>Step 4</h2>
+            Make your first food post and start building your TableTalk network!
+          </Card.Text>
+        </Card.Body>
+           {/* <Card.Img variant="bottom" src="holder.js/100px180" /> */}
+      </Card>
+
+
+
+
+       </>
+     
+
     )
   }
