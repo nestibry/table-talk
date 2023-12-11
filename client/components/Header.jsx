e4b2fad2a7934cf692d57579fb94b6f696901f55
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-12-11 11:10:40 -0600

    heading

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,42 +3,34 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useAppCtx } from "../utils/AppProvider"
-import TTlogo from "../public/src/assets/TableTalk.png"
+import TTlogo from '/assets/TableTalk.png'
 
 export default function Header() {
-  const { user } = useAppCtx()
+    const { user } = useAppCtx()
 
-  return (
-    <div>
-      <a href='https://github.com/lavollmer/songquest'><img src={TTlogo} alt='TT' width="100" height='100'></img></a>
-      <h2>TableTalk</h2>
-      { user?._id !== undefined ? (
-      <Nav.Link href="/logout">Logout</Nav.Link>
-      ):(
-      <Nav.Link href="/auth">Login</Nav.Link>
-      )
-      }
-    </div>
-    // <Navbar expand="lg" className="bg-body-tertiary">
-    //   <Container>
-    //     <Navbar.Brand href="/">My Web Site</Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //     <Navbar.Collapse id="basic-navbar-nav">
-    //       <Nav className="me-auto">
-            
-    //         { user?._id !== undefined && (
-    //           <Nav.Link href="/private">Private Page</Nav.Link>
-    //         )}
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="/"><img src={TTlogo} alt='TTlogo'></img>TableTalk</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
 
-    //         { user?._id !== undefined ? (
-    //           <Nav.Link href="/logout">Logout</Nav.Link>
-    //         ):(
-    //           <Nav.Link href="/auth">Login</Nav.Link>
-    //         )}
-            
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </Container>
-    // </Navbar>
-  );
+                        {user?._id !== undefined ? (
+                            <Nav.Link href="/private">Private Page</Nav.Link>
+                        ) : (
+                            <Nav.Link href="/signup">Sign Up</Nav.Link>
+                        )}
+
+                        {user?._id !== undefined ? (
+                            <Nav.Link href="/logout">Logout</Nav.Link>
+                        ) : (
+                            <Nav.Link href="/auth">Login</Nav.Link>
+                        )}
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
