870531d749a42eac0a5c43737ae7b2bc805cfbb4
nestibry <nestibry@gmail.com>
2023-12-11 09:28:28 -0600

    needed to change the import for PrivatePage back to the PrivatePage.jsx (it was pointing to the ProfilePage.jsx)

--- client/pages/PrivatePage.jsx
+++ client/pages/PrivatePage.jsx
@@ -1,30 +1,17 @@
 import { useEffect, useState } from "react";
 import { useAppCtx } from "../utils/AppProvider";
 
-export default function PrivatePage() {
+export default function ProtectedPage() {
     const appCtx = useAppCtx();
-    const [messageData, setMessageData] = useState({ name: '', phone: '', message: '' });
-
-    // useEffect(() => {
-    //     // Update local state when appCtx.contactFormData changes
-    //     setMessageData(appCtx.contactFormData);
-    // }, [appCtx.contactFormData]);
 
     useEffect(() => {
-        console.log('PrivatePage appCtx:', appCtx);
-        console.log('PrivatePage contactFormData:', appCtx.contactFormData);
-        console.log('PrivatePage messageData:', messageData);
-    }, [appCtx, messageData]);
+        console.log('ProtectPage appCtx:', appCtx);
+    }, [appCtx]);
 
     return (
         <>
             <h1>Private Page</h1>
             <p>This is an example of a page that would require an authenticated user.</p>
-            <div>
-                <strong>Name:</strong> {messageData.name}<br />
-                <strong>Phone:</strong> {messageData.phone}<br />
-                <strong>Message:</strong> {messageData.message}<br />
-            </div>
         </>
     );
 }
