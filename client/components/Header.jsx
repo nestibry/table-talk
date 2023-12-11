654557d5324414f16a44b6c84af85b3b69791129
nestibry <nestibry@gmail.com>
2023-12-11 09:33:00 -0600

    Update Header to include /signup route if undefined user

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,29 +5,31 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useAppCtx } from "../utils/AppProvider"
 
 export default function Header() {
-  const { user } = useAppCtx()
+    const { user } = useAppCtx()
 
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/">My Web Site</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            
-            { user?._id !== undefined && (
-              <Nav.Link href="/private">Private Page</Nav.Link>
-            )}
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="/">My Web Site</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
 
-            { user?._id !== undefined ? (
-              <Nav.Link href="/logout">Logout</Nav.Link>
-            ):(
-              <Nav.Link href="/auth">Login</Nav.Link>
-            )}
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+                        {user?._id !== undefined ? (
+                            <Nav.Link href="/private">Private Page</Nav.Link>
+                        ) : (
+                            <Nav.Link href="/signup">Sign Up</Nav.Link>
+                        )}
+
+                        {user?._id !== undefined ? (
+                            <Nav.Link href="/logout">Logout</Nav.Link>
+                        ) : (
+                            <Nav.Link href="/auth">Login</Nav.Link>
+                        )}
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
