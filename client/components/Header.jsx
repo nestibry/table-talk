d27fe7f2ecb2347c2f787e54b07b2f42558059be
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-12-11 14:47:29 -0600

    Sushi being a Sushi

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,17 +4,27 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useAppCtx } from "../utils/AppProvider"
 import TTlogo from '/assets/TableTalk.png'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import '../public/assets/css/Header.css'
+import NavBar from './Navbar'
 
 export default function Header() {
     const { user } = useAppCtx()
 
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="/"><img src={TTlogo} alt='TTlogo'></img>TableTalk</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+      <>
+        <Navbar className="bg-body-tertiary">
+            <Container className=''>
+                  <Navbar.Brand href="/">
+                    <img src={TTlogo} alt='TTlogo'></img>
+                    <h1>
+                      TableTalk
+                    </h1>
+                  </Navbar.Brand>
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                    <Nav className="">
 
                         {user?._id !== undefined ? (
                             <Nav.Link href="/private">Private Page</Nav.Link>
@@ -27,10 +37,31 @@ export default function Header() {
                         ) : (
                             <Nav.Link href="/auth">Login</Nav.Link>
                         )}
-
                     </Nav>
-                </Navbar.Collapse>
+                  </Navbar.Collapse>
             </Container>
         </Navbar>
+        <Navbar>
+          <Container>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav" className="justify-content-start">
+                    <Nav className="">
+
+                        {user?._id !== undefined ? (
+                            <Nav.Link href="/private">Private Page</Nav.Link>
+                        ) : (
+                            <Nav.Link href="/signup">Sign Up</Nav.Link>
+                        )}
+
+                        {user?._id !== undefined ? (
+                            <Nav.Link href="/logout">Logout</Nav.Link>
+                        ) : (
+                            <Nav.Link href="/auth">Login</Nav.Link>
+                        )}
+                </Nav>
+              </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </>
     );
 }
