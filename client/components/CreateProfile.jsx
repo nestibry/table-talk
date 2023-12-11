2181e35f20d3e67c368ff49977a9f2e96ea9f72a
nestibry <nestibry@gmail.com>
2023-12-11 11:30:21 -0600

    removed cloudinary components for now to get the main functionality working with normal input form fields...will add it back in later

--- client/components/CreateProfile.jsx
+++ client/components/CreateProfile.jsx
@@ -18,17 +18,7 @@ export default function CreateProfile() {
         age: '',
         location_state: '',
         gender_identity: '',
-        status: '',
-
-
-        username: "",
-
-        password: "",
-        name: "",
-        state: "",
-        profileStatus: "",
-        ageGroup: "",
-        profilePicture: null,
+        status: ''
     });
 
     const handleChange = (e) => {
@@ -63,53 +53,6 @@ export default function CreateProfile() {
             console.error("Error!!!!!", error);
         }
     };
-
-
-
-    // Cloudinary config settings
-
-
-    const [publicId, setPublicId] = useState("");
-    // Replace with your own cloud name
-    const [cloudName] = useState("table-talk");
-    // Replace with your own upload preset
-    const [uploadPreset] = useState("wifavopf");
-
-    // Upload Widget Configuration
-    // Remove the comments from the code below to add
-    // additional functionality.
-    // Note that these are only a few examples, to see
-    // the full list of possible parameters that you
-    // can add see:
-    //   https://cloudinary.com/documentation/upload_widget_reference
-
-    const [uwConfig] = useState({
-        cloudName,
-        uploadPreset
-        // cropping: true, //add a cropping step
-        // showAdvancedOptions: true,  //add advanced options (public_id and tag)
-        // sources: [ "local", "url"], // restrict the upload sources to URL and local files
-        // multiple: false,  //restrict upload to a single file
-        // folder: "user_images", //upload files to the specified folder
-        // tags: ["users", "profile"], //add the given tags to the uploaded files
-        // context: {alt: "user_uploaded"}, //add the given context data to the uploaded files
-        // clientAllowedFormats: ["images"], //restrict uploading to image files only
-        // maxImageFileSize: 2000000,  //restrict file size to less than 2MB
-        // maxImageWidth: 2000, //Scales the image down to a width of 2000 pixels before uploading
-        // theme: "purple", //change to a purple theme
-    });
-
-    // Create a Cloudinary instance and set your cloud name.
-    const cld = new Cloudinary({
-        cloud: {
-            cloudName
-        }
-    });
-
-    const myImage = cld.image(publicId);
-
-    //End of Cloudinary config settings
-
 
 
 
@@ -255,26 +198,6 @@ export default function CreateProfile() {
                 </Button>
             </Form>
 
-            <div>
-                <h3>Cloudinary Upload Widget Example</h3>
-                <CloudinaryUploadWidget uwConfig={uwConfig} setPublicId={setPublicId} />
-                <p>
-                    <a
-                        href="https://cloudinary.com/documentation/upload_widget"
-                        target="_blank"
-                    >
-                        Upload Widget User Guide
-                    </a>
-                </p>
-                <p>
-                    <a
-                        href="https://cloudinary.com/documentation/upload_widget_reference"
-                        target="_blank"
-                    >
-                        Upload Widget Reference
-                    </a>
-                </p>
-            </div>
         </>
     );
 }
