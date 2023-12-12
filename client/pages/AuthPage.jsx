0f40c31e838c2db20064d9e2363cc76b8c818a00
nestibry <nestibry@gmail.com>
2023-12-12 14:02:34 -0600

    CreateProfile, moved the Upload Photo to the top of the form"

--- client/pages/AuthPage.jsx
+++ client/pages/AuthPage.jsx
@@ -2,7 +2,8 @@ import {Auth, CreateProfile} from "../components/"
 
 export default function AuthPage({ type = "login" }) {
     return (
-        <div className="d-flex gap-5">
+        // <div className="d-flex gap-5">
+        <div className="container">
             {type === 'login' ?
                 <div>
                     <Auth />
