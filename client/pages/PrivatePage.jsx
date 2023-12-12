5f2a7841340d9580dd7aa209e39a445bcf03951e
nestibry <nestibry@gmail.com>
2023-12-12 00:09:34 -0600

    init fetchPosts()...working on the sorting

--- client/pages/PrivatePage.jsx
+++ client/pages/PrivatePage.jsx
@@ -4,8 +4,41 @@ import { useAppCtx } from "../utils/AppProvider";
 export default function ProtectedPage() {
     const appCtx = useAppCtx();
 
+    const [posts, setPosts] = useState({});
+
+    async function fetchPosts() {
+        try {
+            const socialsResponse = await fetch('/api/social');
+            const socialsResponseJson = await socialsResponse.json();
+            // console.log('Socials Response',socialsResponse);
+            // console.log('Socials JSON', socialsResponseJson); 
+            
+            const reviewsResponse = await fetch('/api/review');
+            const reviewsResponseJson = await reviewsResponse.json();
+            // console.log('reviews Response',reviewsResponse);
+            // console.log('reviews JSON', reviewsResponseJson); 
+            
+            if (socialsResponseJson.result === "success" && reviewsResponseJson.result) {
+                const socials = socialsResponseJson.payload;
+                const reviews = reviewsResponseJson.payload;
+                const newsFeed = [];
+                console.log('Socials JSON', socialsResponseJson.payload);
+                console.log('Reviews JSON', reviewsResponseJson.payload); 
+                socials.forEach(item => { newsFeed.push(item)});
+                reviews.forEach(item => { newsFeed.push(item)});
+                console.log(newsFeed);  
+                const timestamps = newsFeed.map(item => item.createdAt);
+                console.log(timestamps);
+            } 
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     useEffect(() => {
-        console.log('ProtectPage appCtx:', appCtx);
+        console.log('appCtx:', appCtx);
+        fetchPosts();        
     }, [appCtx]);
 
     return (
