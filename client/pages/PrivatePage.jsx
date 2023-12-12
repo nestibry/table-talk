57ad5e0e2471ac791130e86f213e796edd1a0ea5
nestibry <nestibry@gmail.com>
2023-12-12 01:09:30 -0600

    post description and _id rendered on page

--- client/pages/PrivatePage.jsx
+++ client/pages/PrivatePage.jsx
@@ -4,39 +4,24 @@ import { useAppCtx } from "../utils/AppProvider";
 export default function ProtectedPage() {
     const appCtx = useAppCtx();
 
-    const [posts, setPosts] = useState({});
+    const [posts, setPosts] = useState();
 
     async function fetchPosts() {
         try {
             const socialsResponse = await fetch('/api/social');
             const socialsResponseJson = await socialsResponse.json();
-            // console.log('Socials Response',socialsResponse);
-            // console.log('Socials JSON', socialsResponseJson); 
 
             const reviewsResponse = await fetch('/api/review');
             const reviewsResponseJson = await reviewsResponse.json();
-            // console.log('reviews Response',reviewsResponse);
-            // console.log('reviews JSON', reviewsResponseJson); 
 
             if (socialsResponseJson.result === "success" && reviewsResponseJson.result === "success") {
                 const socials = socialsResponseJson.payload;
                 const reviews = reviewsResponseJson.payload;
-                const newsFeed = [];
-                // console.log('Socials JSON', socialsResponseJson.payload);
-                // console.log('Reviews JSON', reviewsResponseJson.payload); 
-                socials.forEach(item => { newsFeed.push(item) });
-                reviews.forEach(item => { newsFeed.push(item) });
-                console.log('News Feed', newsFeed);
 
-                // const timestamps = newsFeed.map(item => item.createdAt);
-                // console.log(timestamps);
-                // const sorted = [...timestamps].sort((a, b) => new Date(a) - new Date(b));
-                // console.log(sorted);
-
-                // const sortedNewsFeed = [...newsFeed].sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
-                const sortedNewsFeed = [...newsFeed].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+                // Create and Sort the NewsFeed
+                const newsFeed = [...socials, ...reviews];
+                const sortedNewsFeed = newsFeed.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
                 console.log('Sorted News Feed', sortedNewsFeed);
-
                 setPosts(sortedNewsFeed);
             }
 
@@ -46,16 +31,23 @@ export default function ProtectedPage() {
     }
 
     useEffect(() => {
-        console.log('appCtx:', appCtx);
         fetchPosts();
     }, []);
 
     return (
-        <>
-            <div>
-                <h1>Private Page</h1>
-                <p>This is an example of a page that would require an authenticated user.</p>
-            </div>
-        </>
+        <div>
+            <h1>News Feed</h1>
+            {posts ? (
+                posts.map(post => (
+                    <div key={post._id}>
+                        <p>{post.description}</p>
+                        <p>{post._id}</p>
+                    </div>
+                ))
+            ) : (
+                <p>Loading...</p>
+            )}
+            <p>This is an example of a page that would require an authenticated user.</p>
+        </div>
     );
 }
