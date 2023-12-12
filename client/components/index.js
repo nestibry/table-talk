4d34cd6b321469b157f5d1ff799726a9509f0c1e
nestibry <nestibry@gmail.com>
2023-12-12 13:35:56 -0600

    updated components index.js to include CreateProfile and updated AuthPage

--- client/components/index.js
+++ client/components/index.js
@@ -1,9 +1,10 @@
 
 
 // Application Components
-export { default as Auth } from './Auth'
-export { default as Header } from './Header.jsx'
-export { default as ProtectedRoute } from './ProtectedRoute'
+export { default as Auth } from './Auth';
+export { default as Header } from './Header.jsx';
+export { default as ProtectedRoute } from './ProtectedRoute';
+export { default as CreateProfile } from './CreateProfile.jsx';
 
 
 // Development Components
--- client/pages/AuthPage.jsx
+++ client/pages/AuthPage.jsx
@@ -1,11 +1,6 @@
-import { useState, useEffect } from "react"
-import Auth from "../components/Auth"
-import CreateProfile from "../components/CreateProfile"
+import {Auth, CreateProfile} from "../components/"
 
-// export default function AuthPage(){
 export default function AuthPage({ type = "login" }) {
-
-
     return (
         <div className="d-flex gap-5">
             {type === 'login' ?
