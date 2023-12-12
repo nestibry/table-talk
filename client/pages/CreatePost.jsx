1c0dcd8cb9fe618ebc21411140a42e4982ef2442
Veronica Perez <veperez@live.com>
2023-12-12 12:19:09 -0600

    updating posting page

--- client/pages/CreatePost.jsx
+++ client/pages/CreatePost.jsx
@@ -1,48 +1,19 @@
-import Card from "@mui/material/Card";
-import Button from "@mui/material/Button";
-import CardContent from "@mui/material/CardContent";
+import * as React from "react";
+import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import CloudinaryUploadWidget from "../components/CloudinaryUploadWidget";
+// import CloudinaryUploadWidget from "../components/CloudinaryUploadWidget";
+//add in upload photo option to form field
+// import PostCard from "../components/PostCard";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
 
 
-
-
-
-//from mui textfields
-export default function () {
+export default function CreatePost() {
   return (
     <>
-      <h1>Create a Post</h1>
-      <Card>
-        <CardContent>
-          <TextField
-            label="Description"
-            variant="outlined"
-            name="description"
-            value={newPost.description}
-            onChange={handleInputChange}
-          />
-          <TextField
-            label="Restaurant Name"
-            variant="outlined"
-            name="restaurant_name"
-            value={newPost.restaurant_name}
-            onChange={handleInputChange}
-          />
-          <TextField
-            label="Restaurant City"
-            variant="outlined"
-            name="restaurant_city"
-            value={newPost.restaurant_city}
-            onChange={handleInputChange}
-          />
-           <input type="file" accept="image/*" onChange={handlePhotoChange} />
-        </CardContent>
-
-        <Button onClick={handleCreatePost} variant="contained" color="primary">
-          Create Post
-        </Button>
-      </Card>
+      
     </>
   );
 }
