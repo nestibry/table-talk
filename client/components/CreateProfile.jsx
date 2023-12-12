853a075d822ae0945044b627987bd611a437715e
nestibry <nestibry@gmail.com>
2023-12-11 18:45:06 -0600

    init handleFormChange and handleFormSubmit

--- client/components/CreateProfile.jsx
+++ client/components/CreateProfile.jsx
@@ -21,181 +21,150 @@ export default function CreateProfile() {
         status: ''
     });
 
-    const handleChange = (e) => {
-        const { name, value, type, checked, files } = e.target;
+    const handleFormChange = (e) => {
+        setFormData({...formData, [e.target.name]:e.target.value});
+    }
 
-        const newValue = type === "checkbox" ? checked : files ? files[0] : value;
-
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: newValue,
-        }));
-    };
-
-    const handleSubmit = async (e) => {
+    const handleFormSubmit = async (e) => {
         e.preventDefault();
 
-        try {
-            const response = await fetch("your-server-endpoint", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(formData),
-            });
+        console.log('Form Submitted:', formData);
 
-            if (response.ok) {
-                console.log("Your profile has been created!!!");
-            } else {
-                console.error("Profile not created!");
-            }
-        } catch (error) {
-            console.error("Error!!!!!", error);
-        }
+        // try {
+        //     const response = await fetch("your-server-endpoint", {
+        //         method: "POST",
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //         },
+        //         body: JSON.stringify(formData),
+        //     });
+
+        //     if (response.ok) {
+        //         console.log("Your profile has been created!!!");
+        //     } else {
+        //         console.error("Profile not created!");
+        //     }
+        // } catch (error) {
+        //     console.error("Error!!!!!", error);
+        // }
     };
 
 
 
     return (
         <>
-            <Form onSubmit={handleSubmit}>
-                <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="email" placeholder="Enter username" />
-                    </Form.Group>
 
+            {/* 
+            [x] email: '',
+            [x] display_name: '',
+            [x] password: '',
 
+            [x] status: '' 
+            [x] location_state: '',
+            [x] age: '',
+            gender_identity: '',
+            profile_pic: '',
+        */}
+            <Form onSubmit={handleFormSubmit} onChange={handleFormChange}>
 
-                    <Form.Group as={Col} controlId="formGridPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                </Row>
-                <Form.Group as={Col} controlId="formGridEmail">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                        type="email"
-                        placeholder="Enter first name (last name optional)"
-                    />
+                <Form.Group className="mb-3" controlId="formEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="name@example.com" />
                 </Form.Group>
-                {/* <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>Address</Form.Label>
-        <Form.Control placeholder="1234 Main St" />
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Address 2</Form.Label>
-        <Form.Control placeholder="Apartment, studio, or floor" />
-      </Form.Group> */}
+                <Form.Group className="mb-3" controlId="formDisplayName">
+                    <Form.Label>Display Name</Form.Label>
+                    <Form.Control type="text" name="display_name" placeholder="YourDisplayName" />
+                </Form.Group>
 
-                <Row className="mb-3">
-                    {/* <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>City</Form.Label>
-            <Form.Control />
-          </Form.Group> */}
+                <Form.Group className="mb-3" controlId="formPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Password" />
+                </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridState">
-                        <Form.Label>State</Form.Label>
-                        <Form.Select
-                            name="state"
-                            value={formData.state}
-                            onChange={handleChange}
-                        >
-                            <option value="">Choose...</option>
-                            <option value="AL">Alabama</option>
-                            <option value="AK">Alaska</option>
-                            <option value="AZ">Arizona</option>
-                            <option value="AR">Arkansas</option>
-                            <option value="CA">California</option>
-                            <option value="CO">Colorado</option>
-                            <option value="CT">Connecticut</option>
-                            <option value="DE">Delaware</option>
-                            <option value="FL">Florida</option>
-                            <option value="GA">Georgia</option>
-                            <option value="HI">Hawaii</option>
-                            <option value="ID">Idaho</option>
-                            <option value="IL">Illinois</option>
-                            <option value="IN">Indiana</option>
-                            <option value="IA">Iowa</option>
-                            <option value="KS">Kansas</option>
-                            <option value="KY">Kentucky</option>
-                            <option value="LA">Louisiana</option>
-                            <option value="ME">Maine</option>
-                            <option value="MD">Maryland</option>
-                            <option value="MA">Massachusetts</option>
-                            <option value="MI">Michigan</option>
-                            <option value="MN">Minnesota</option>
-                            <option value="MS">Mississippi</option>
-                            <option value="MO">Missouri</option>
-                            <option value="MT">Montana</option>
-                            <option value="NE">Nebraska</option>
-                            <option value="NV">Nevada</option>
-                            <option value="NH">New Hampshire</option>
-                            <option value="NJ">New Jersey</option>
-                            <option value="NM">New Mexico</option>
-                            <option value="NY">New York</option>
-                            <option value="NC">North Carolina</option>
-                            <option value="ND">North Dakota</option>
-                            <option value="OH">Ohio</option>
-                            <option value="OK">Oklahoma</option>
-                            <option value="OR">Oregon</option>
-                            <option value="PA">Pennsylvania</option>
-                            <option value="RI">Rhode Island</option>
-                            <option value="SC">South Carolina</option>
-                            <option value="SD">South Dakota</option>
-                            <option value="TN">Tennessee</option>
-                            <option value="TX">Texas</option>
-                            <option value="UT">Utah</option>
-                            <option value="VT">Vermont</option>
-                            <option value="VA">Virginia</option>
-                            <option value="WA">Washington</option>
-                            <option value="WV">West Virginia</option>
-                            <option value="WI">Wisconsin</option>
-                            <option value="WY">Wyoming</option>
-                        </Form.Select>
-                    </Form.Group>
+                <Form.Group className="mb-3" controlId="formProfileStatus">
+                    <Form.Label>Profile Status</Form.Label>
+                    <Form.Select name="status">
+                        <option>Select Status</option>
+                        <option value="1">Looking for new friends</option>
+                        <option value="2">Looking for a romantic connection</option>
+                        <option value="3">Just here for the food recommendations</option>
+                    </Form.Select>
+                </Form.Group>
 
-                    {/* <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control />
-          </Form.Group> */}
-                </Row>
-                {/* 
-        <Form.Group className="mb-3" id="formGridCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group> */}
+                <Form.Group className="mb-3" controlId="formLocationState">
+                    <Form.Label>State</Form.Label>
+                    <Form.Select name="location_state">
+                        <option value="">Select State</option>
+                        <option value="AL">Alabama</option>
+                        <option value="AK">Alaska</option>
+                        <option value="AZ">Arizona</option>
+                        <option value="AR">Arkansas</option>
+                        <option value="CA">California</option>
+                        <option value="CO">Colorado</option>
+                        <option value="CT">Connecticut</option>
+                        <option value="DE">Delaware</option>
+                        <option value="FL">Florida</option>
+                        <option value="GA">Georgia</option>
+                        <option value="HI">Hawaii</option>
+                        <option value="ID">Idaho</option>
+                        <option value="IL">Illinois</option>
+                        <option value="IN">Indiana</option>
+                        <option value="IA">Iowa</option>
+                        <option value="KS">Kansas</option>
+                        <option value="KY">Kentucky</option>
+                        <option value="LA">Louisiana</option>
+                        <option value="ME">Maine</option>
+                        <option value="MD">Maryland</option>
+                        <option value="MA">Massachusetts</option>
+                        <option value="MI">Michigan</option>
+                        <option value="MN">Minnesota</option>
+                        <option value="MS">Mississippi</option>
+                        <option value="MO">Missouri</option>
+                        <option value="MT">Montana</option>
+                        <option value="NE">Nebraska</option>
+                        <option value="NV">Nevada</option>
+                        <option value="NH">New Hampshire</option>
+                        <option value="NJ">New Jersey</option>
+                        <option value="NM">New Mexico</option>
+                        <option value="NY">New York</option>
+                        <option value="NC">North Carolina</option>
+                        <option value="ND">North Dakota</option>
+                        <option value="OH">Ohio</option>
+                        <option value="OK">Oklahoma</option>
+                        <option value="OR">Oregon</option>
+                        <option value="PA">Pennsylvania</option>
+                        <option value="RI">Rhode Island</option>
+                        <option value="SC">South Carolina</option>
+                        <option value="SD">South Dakota</option>
+                        <option value="TN">Tennessee</option>
+                        <option value="TX">Texas</option>
+                        <option value="UT">Utah</option>
+                        <option value="VT">Vermont</option>
+                        <option value="VA">Virginia</option>
+                        <option value="WA">Washington</option>
+                        <option value="WV">West Virginia</option>
+                        <option value="WI">Wisconsin</option>
+                        <option value="WY">Wyoming</option>
+                    </Form.Select>
+                </Form.Group>
 
-                <Form.Select aria-label="Default select example">
-                    <option>Choose a Profile Status</option>
-                    <option value="1">Looking for new friends</option>
-                    <option value="2">Looking for a romantic connection</option>
-                    <option value="3">Just here for the food recommendations</option>
-                </Form.Select>
-
-                <Form.Select aria-label="Default select example">
-                    <option>Age</option>
-                    <option value="1">18-25</option>
-                    <option value="2">26-35</option>
-                    <option value="3">36-45</option>
-                    <option value="4">46-60</option>
-                    <option value="5">60+</option>
-                </Form.Select>
-
-                {/* <Form.Group controlId="formFile" className="mb-3">
-            <Form.Label>Upload a profile pict</Form.Label>
-            <Form.Control type="file" />
-          </Form.Group> */}
-
-
-
-                {/* <Form.Group className="mb-3" id="formGridCheckbox">
-            <Form.Check type="checkbox" label="I am over 18 years of age." />
-          </Form.Group> */}
+                <Form.Group className="mb-3" controlId="formAge">
+                    <Form.Label>Age</Form.Label>
+                    <Form.Select name="age">
+                        <option>Select Age Range</option>
+                        <option value="1">18-25</option>
+                        <option value="2">26-35</option>
+                        <option value="3">36-45</option>
+                        <option value="4">46-60</option>
+                        <option value="5">60+</option>
+                    </Form.Select>
+                </Form.Group>
 
                 <Button variant="primary" type="submit">
                     Create Profile
                 </Button>
+
             </Form>
 
         </>
