293112395f4257058f88002d94b42ce228042746
nestibry <nestibry@gmail.com>
2023-12-11 21:39:46 -0600

    from Header.jsx, removed the second Navbar container, mostly is a duplicate of the first container but without the Table Talk image and brand....the second container was causing errors with the login/signup functionality

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -12,55 +12,35 @@ import NavBar from './Navbar'
 export default function Header() {
     const { user } = useAppCtx()
 
-  return (
-    <>
-      <Navbar className="bg-body-tertiary">
-        <Container className=''>
-          <Navbar.Brand href="/">
-            <img src={TTlogo} alt='TTlogo'></img>
-            <h1>
-              TableTalk
-            </h1>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-            <Nav className="">
+    return (
+        <>
+            <Navbar className="bg-body-tertiary">
+                <Container className=''>
+                    <Navbar.Brand href="/">
+                        <img src={TTlogo} alt='TTlogo'></img>
+                        <h1>
+                            TableTalk
+                        </h1>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                        <Nav className="">
 
-              {user?._id !== undefined ? (
-                <Nav.Link href="/private">Private Page</Nav.Link>
-              ) : (
-                <Nav.Link href="/signup">Sign Up</Nav.Link>
-              )}
+                            {user?._id !== undefined ? (
+                                <Nav.Link href="/private">Private Page</Nav.Link>
+                            ) : (
+                                <Nav.Link href="/signup">Sign Up</Nav.Link>
+                            )}
 
-              {user?._id !== undefined ? (
-                <Nav.Link href="/logout">Logout</Nav.Link>
-              ) : (
-                <Nav.Link href="/auth">Login</Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-      <Navbar>
-        <Container>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-start">
-            <Nav className="">
-
-              {user?._id !== undefined ? (
-                <Nav.Link href="/private">Private Page</Nav.Link>
-              ) : (
-                <Nav.Link href="/signup">Sign Up</Nav.Link>
-              )}
-              {user?._id !== undefined ? (
-                <Nav.Link href="/logout">Logout</Nav.Link>
-              ) : (
-                <Nav.Link href="/auth">Login</Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
-  );
+                            {user?._id !== undefined ? (
+                                <Nav.Link href="/logout">Logout</Nav.Link>
+                            ) : (
+                                <Nav.Link href="/auth">Login</Nav.Link>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
