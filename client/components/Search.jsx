5f28aa6249fb3aa4120fdf82f389d07f3a714868
unknown <mhalder9555@gmail.com>
2023-12-13 14:49:50 -0600

    Search styling and cleanup finished.

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,15 +1,32 @@
-
+import { Avatar } from "@mui/material";
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Search(props) {
 
 
   return (
     <>
-      <ul>
-        <li>profile_pic:{props.profile_pic}</li>
-        <li>display_name:{props.display_name}</li>
-        <li>status:{props.status}</li>
-      </ul>
+
+      <Container>
+        <Col className="mt-4" md={{ span: 4, offset: 4 }}>
+          <Card bg="secondary">
+            <Card.Body>
+              <Row>
+                <Col md={{ span: 3, offset: 2 }} className="d-flex align-items-center">
+                  <Avatar alt="User Profile Pic" src={props.profile_pic} sx={{ bgcolor: "ffffff" }}></Avatar>
+                </Col>
+                <Col>
+                  <p>{props.display_name}</p>
+                  <p>{props.status}</p>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Container>
     </>
   );
 }
